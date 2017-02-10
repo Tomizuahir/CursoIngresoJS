3ce2649580f80dd4edd401ad9f2dfff475c02a5c
Tomizuahir <tomashiese@gmail.com>
2017-02-09 22:07:50 -0300

    commit 6
    Esto se hizo en clases

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,6 +1,20 @@
 function Mostrar()
 {
-//tomo la edad  
+	//tomo la edad  
+	var edad;
+	edad=document.getElementById('edad').value;
+	if(edad>17)
+	{
+		alert("mayor de edad");
+	}
+	if(edad<18 && edad>12)
+	{
+		alert("adolescente");
+	}
+	if(edad<13)
+	{
+		alert("niño");
+	}
 
 
 
--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -1,6 +1,17 @@
 function Mostrar()
 {
-//tomo la edad  
+	//tomo la edad  
+	var edad;
+	var estadocivil;
+	edad=document.getElementById('edad').value;
+	estadocivil.document.getElementById('estadoCivil').value;
+	
+	if(edad<18 && estadocivil=="soltero")
+	{
+	  alert("es muy pequeño para no ser soltero");
+	}
+	
+
 
 	
 
