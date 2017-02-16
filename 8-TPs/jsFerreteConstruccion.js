5f82a9a6aae67d4d820a25688471cb7b1310090b
Tomizuahir <tomashiese@gmail.com>
2017-02-16 19:06:15 -0300

    commit 8
    Esto se hizo en clases

--- 8-TPs/jsFerreteConstruccion.js
+++ 8-TPs/jsFerreteConstruccion.js
@@ -12,7 +12,7 @@ function Rectangulo ()
 	largodelterreno=document.getElementById('Largo').value;
 	anchodelterreno=document.getElementById('Ancho').value;
 	resultado=largodelterreno*anchodelterreno*3;
-	alert(resultado);
+	alert("Se necesita de alambre" + resultado);
 
 
 }
@@ -23,23 +23,24 @@ function Circulo ()
 	radio=document.getElementById('Radio').value;
 	radio=3.14
 	resultado=radio*3;
-	alert(resultado);
+	alert("Se necesita de alambre" + resultado);
 
 }
 function Materiales () 
 {
 	var largodelterreno;
 	var anchodelterreno;
-	var metros;
 	var cemento;
 	var cal;
 	var resultado;
 	largodelterreno=document.getElementById('Largo').value;
 	anchodelterreno=document.getElementById('Ancho').value;
-	metros=1*1;
 	cemento=2;
 	cal=3;
-	resultado=metros*largodelterreno*anchodelterreno;
+	resultado=largodelterreno*anchodelterreno*cemento;
+	alert("Se necesitan de cemento"  +  resultado);
+	resultado=largodelterreno*anchodelterreno*cal;
+	alert("Se necesitan de cal"  +  resultado);
 	
 
 }
