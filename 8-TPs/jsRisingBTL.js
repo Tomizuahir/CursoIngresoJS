677d6fc465a7d37b163c1dba1a75f18c1ac2924f
Tomizuahir <tomashiese@gmail.com>
2017-07-14 16:16:48 -0300

    No pude hacer el numero de legajo

--- 8-TPs/jsRisingBTL.js
+++ 8-TPs/jsRisingBTL.js
@@ -9,10 +9,63 @@ F.	Nacionalidad, “A” para argentinos, “E” para extranjeros, “N” para
  */
 function ComenzarIngreso () 
 {
+ 	var edad;
+ 	var sexo;
+ 	var estadocivil;
+ 	var sueldobruto;
+ 	var numerolegajo;
+ 	var nacionalidad;
+ 	edad=prompt("ingrese su edad");
+ 	sexo=prompt("ingrese su sexo f para femenino o m para masculino");
+ 	estadocivil=prompt("ingrese su estado civil 1 para soltero 2 para casado 3 para divorciado o 4 para viudo");
+ 	sueldobruto=prompt("ingrese su sueldo");
+ 	numerolegajo=prompt("ingrese su numero de legajo sin ceros a la izquierda");
+ 	nacionalidad=prompt("ingrese su nacionalidad A para argentino E para extrajero o N para nacionalizado");
  
- {
- 	
- 	
- }
+ 	while(!(edad>17 && edad<91))
+ 	{
+ 		alert("reintente por favor");
+ 		edad=prompt("reingrese edad");
+ 	}
+ 	while(!(sexo=="m" || sexo=="f"))
+ 	{
+ 		alert("reintente por favor")
+ 		sexo=prompt("reingrese su sexo f para femenino o m para masculino");
+ 	}
+ 	while(!(estadocivil==1 ||  estadocivil==2 ||  estadocivil==3 ||  estadocivil ==4))
+ 	{
+ 		alert("reintente por favor")
+ 		estadocivil=prompt("reingrese su estado civil");
+ 	}
+ 	while(sueldobruto<7999)
+ 	{
+ 		alert("reintente por favor");
+ 		sueldobruto=prompt("reingrese su sueldo");
+ 	}
+ 	 while ()
+ 	 { 
+ 	 	alert("reintente por favor");
+ 	 	numerolegajo=prompt("reingrese su numero de legajo sin ceros a la izquierda");
+ 	 }
+
+
+
+ 	while(!(nacionalidad=="A" || nacionalidad=="E" || nacionalidad=="N"))
+ 	{
+ 		alert("reintente por favor");
+ 		nacionalidad=prompt("reingrese su nacionalidad A para argentino E para extrajero o N para nacionalizado");
+ 	}
+
+ 
+  document.getElementById('Edad').value=edad;
+  document.getElementById('Sexo').value=sexo;
+  document.getElementById('EstadoCivil').value=estadocivil;
+  document.getElementById('Sueldo').value=sueldobruto;
+  document.getElementById('Legajo').value=numerolegajo;
+  document.getElementById('Nacionalidad').value=nacionalidad;
+
+
+
+
   alert("bienvenido");
 }
